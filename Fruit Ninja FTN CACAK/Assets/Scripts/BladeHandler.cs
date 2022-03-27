caeb13ec13ea90d9d5df8be96758646afd6bf8de
MarkoJoker <djordjevicit97@gmail.com>
2022-03-27 01:07:26 +0100

    FRUIT NINJA

---
+++ Fruit Ninja FTN CACAK/Assets/Scripts/BladeHandler.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class BladeHandler : MonoBehaviour {
+
+    private bool isCutting;
+    private Rigidbody2D rigidBody; // enable GameObject to act, to make gameObject move in realistic way !
+    private Camera mainCamera;
+    private CircleCollider2D circleCollider; // interacts with the 2D physics system
+    private Vector2 previousPosition; // used to calculate speed of blade
+    public float minSpeed; // blade has to move this fast to cut things
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+        rigidBody = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCut();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopCut();
+        }
+
+        if (isCutting)
+        {
+            UpdateBlade();
+        }
+	}
+
+    private void UpdateBlade() {
+        // update blade position to where the mouse is in the world
+        Vector2 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        rigidBody.position = newPosition;
+        // calculate velocity of blade
+        float velocity = (newPosition - previousPosition).magnitude * Time.deltaTime;
+        // allow blade to cut things if its reached minimum required velocity
+        circleCollider.enabled = (velocity > minSpeed) ? true : false;
+        previousPosition = newPosition;
+    }
+
+    private void StartCut() {
+        isCutting = true;
+        previousPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void StopCut() {
+        isCutting = false;
+        circleCollider.enabled = false;
+    }
+}
---
+++ Fruit Ninja FTN CACAK/Assets/Scripts/GameManager.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public ObjectPooler[] fruitPool;
+    public ObjectPooler bombPool;
+    public ObjectPooler powerupPool;
+    public Transform[] spawnPoints;
+    private ScoreManager scoreManager;
+    public AudioSource[] squishSound;
+    public AudioSource bombSound;
+    public AudioSource powerupSound;
+    public float spawnDelay; // how long between each fruit spawn;
+    public float delayVariance; // determine the random range of spawn delays
+    public float delayMultiplier; // fruits will start spawning faster and faster
+    public int delayThreshold; // how many fruits needed to be cut before decreasing spawn time
+    public int bombChance;
+    public int bombChanceLimit; // bombs spawn rate will cap out at this limit
+    public int powerupChance;
+    private int fruitsCut;
+    public bool gameOver;
+
+    public GameObject panelGameOver;
+    public GameObject homePanel;
+    public GameObject introductionPanel;
+
+	// Use this for initialization
+	void Start () {
+        homePanel.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public void ActivateBomb() {
+        if (bombSound.isPlaying)
+        {
+            bombSound.Stop();
+        }
+        bombSound.Play();
+        scoreManager.ResetScore();
+        gameOver = true;
+        StopAllCoroutines();
+        panelGameOver.SetActive(true);
+    }
+
+    public void CutFruit() {
+        scoreManager.AddScore();
+        int sound = Random.Range(0, squishSound.Length);
+        if (squishSound[sound].isPlaying)
+        {
+            squishSound[sound].Stop(); // prevent overloading of sounds 
+        }
+        squishSound[sound].Play();
+
+        // prevent overflow, but players shouldnt really get to 2^31 fruits anyway
+        if (++fruitsCut == delayThreshold) {
+            fruitsCut = 0; 
+            spawnDelay *= delayMultiplier;
+            if (bombChance < bombChanceLimit)
+            {
+                bombChance += 2;
+            }
+        }
+    }
+
+    private void Spawn() {
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        int chance = Random.Range(0, 100 + powerupChance); // extra % used to determine powerup spawn
+        GameObject spawnedObject;
+        if (chance <= bombChance)
+            spawnedObject = bombPool.GetPooledObject();
+        else if (chance > bombChance && chance <= 100)
+            spawnedObject = fruitPool[Random.Range(0, fruitPool.Length)].GetPooledObject();
+        else
+            spawnedObject = powerupPool.GetPooledObject();
+        spawnedObject.transform.position = spawnPoint.position;
+        spawnedObject.transform.rotation = spawnPoint.rotation;
+        spawnedObject.SetActive(true);
+    }
+
+    IEnumerator SpawnObjects() {
+        while (true) {
+            if (!gameOver) {
+                float delay = Random.Range(spawnDelay * delayVariance, spawnDelay / delayVariance);
+                yield return new WaitForSeconds(delay);
+                Spawn(); // fruit, bomb, or powerup;
+            }
+            else {
+                yield return new WaitForSeconds(3f);
+                gameOver = false;
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        homePanel.SetActive(false);
+        fruitsCut = 0;
+
+        scoreManager = FindObjectOfType<ScoreManager>();
+        StartCoroutine(SpawnObjects());
+    }
+
+    public void RestartGame()
+    {
+        gameOver = false;
+        StartCoroutine(SpawnObjects());
+        panelGameOver.SetActive(false);
+    }
+
+    public void BackToHomePannel()
+    {
+        StopAllCoroutines();
+        scoreManager.ResetScore();
+        homePanel.SetActive(true);
+    }
+
+    public void IntroductionIN()
+    {
+        homePanel.SetActive(false);
+        introductionPanel.SetActive(true);
+
+    }
+
+    public void IntroductionOUT()
+    {
+        introductionPanel.SetActive(false);
+        homePanel.SetActive(true);  
+    }
+
+    public void ExitGame()
+    {
+        StopAllCoroutines();
+        Application.Quit();
+    }
+}
